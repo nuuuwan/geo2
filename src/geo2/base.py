da57ca84c5735484486b35b2207b8a8b3f977cf3
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-07-03 10:20:45 +0530

    Initial Cleanup

--- src/geo2/base.py
+++
@@ -1,4 +0,0 @@
-def rangef(start, end, step):
-    n_steps = (int)(round(1.0 * (end - start) / step))
-    for i in range(n_steps):
-        yield start + i * step
--- src/geo2/common.py
+++
@@ -1,8 +0,0 @@
-import os
-
-DIR_DATA = '/tmp/geo2.data'
-
-
-def init_dir_data():
-    if not os.path.exists(DIR_DATA):
-        os.mkdir(DIR_DATA)
