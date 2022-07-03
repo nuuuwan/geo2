b5f3d2081a996d2f95647f43a52e764afc96bdb5
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-07-03 09:03:14 +0530

    Moved region_entity_type into tree generation

--- src/geo2/gbox_tree.py
+++ src/geo2/gbox_tree.py
@@ -2,15 +2,18 @@ import os
 
 from utils import JSONFile, logx
 
-from geo2 import gbox
+from geo2 import gbox, regionx
 
-log = logx.get_logger('geo2.gbox')
+log = logx.get_logger('geo2.gbox_tree')
 
 
-def store_tree_file(region_to_geo):
+def store_tree_file(region_entity_type):
+    log.debug(f'{region_entity_type=}')
+    region_to_geo = regionx.get_region_to_geo(region_entity_type)
     root = gbox.GBox.root()
     tree = root.get_tree(region_to_geo)
-    tree_file = '/tmp/geo2.tree.json'
+
+    tree_file = f'/tmp/geo2.tree.{region_entity_type}.json'
     JSONFile(tree_file).write(tree)
     n_tree_file = os.path.getsize(tree_file) / 1_000_000
     log.info(f'Wrote {tree_file} ({n_tree_file:.2f}MB)')
@@ -19,7 +22,4 @@ def store_tree_file(region_to_geo):
 
 
 if __name__ == '__main__':
-    from geo2 import regionx
-    region_entity_type = 'province'
-    region_to_geo = regionx.get_region_to_geo(region_entity_type)
-    store_tree_file(region_to_geo)
+    store_tree_file('province')
