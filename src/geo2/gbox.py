4234c88d03146e3337692f97de85ffb76bedb353
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-07-03 08:09:08 +0530

    Split out base methods to GBoxBase

--- src/geo2/gbox.py
+++ src/geo2/gbox.py
@@ -1,9 +1,9 @@
 import os
-from functools import cache, cached_property
+from functools import cached_property
 
 from utils import JSONFile, logx
 
-from geo2 import core
+from geo2 import core, gbox_base
 
 log = logx.get_logger('geo2.gbox')
 
@@ -11,91 +11,7 @@ MIN_PREC = 0.9 / 16
 SPLITS = 2
 
 
-LATLNG0 = [79, 5]
-SPAN0 = 5
-
-
-class GBox:
-    def __init__(self, ixiy, n):
-        self.ixiy = ixiy
-        self.n = n
-
-    @cached_property
-    def lng0(self):
-        return LATLNG0[0]
-
-    @cached_property
-    def lat0(self):
-        return LATLNG0[1]
-
-    @cached_property
-    def ix(self):
-        return self.ixiy[0]
-
-    @cached_property
-    def iy(self):
-        return self.ixiy[1]
-
-    @cached_property
-    def prec(self):
-        return 1.0 * SPAN0 / self.n
-
-    @cached_property
-    def dlng(self):
-        return self.ix * self.prec
-
-    @cached_property
-    def dlat(self):
-        return self.iy * self.prec
-
-    @cached_property
-    def min_lng(self):
-        return self.lng0 + self.dlng
-
-    @cached_property
-    def min_lat(self):
-        return self.lat0 + self.dlat
-
-    @cached_property
-    def max_lng(self):
-        return self.min_lng + self.prec
-
-    @cached_property
-    def max_lat(self):
-        return self.min_lat + self.prec
-
-    @cached_property
-    def min_lnglat(self):
-        return [self.min_lng, self.min_lat]
-
-    @cached_property
-    def max_lnglat(self):
-        return [self.max_lng, self.max_lat]
-
-    @cached_property
-    def mid_lng(self):
-        return self.min_lng + self.prec / 2
-
-    @cached_property
-    def mid_lat(self):
-        return self.min_lat + self.prec / 2
-
-    @cached_property
-    def mid_lnglat(self):
-        return [self.mid_lng, self.mid_lat]
-
-    @cache
-    def __str__(self):
-        return f'{self.ix}:{self.iy}:{self.n}'
-
-    def to_str(self):
-        return str(self)
-
-    @staticmethod
-    def from_str(s):
-        [ix, iy, n] = [(int)(si) for si in s.split(':')]
-        return GBox([ix, iy], n)
-
+class GBox(gbox_base.GBoxBase):
     def contains_lnglat(self, lnglat):
         lng, lat = lnglat
         return all(
