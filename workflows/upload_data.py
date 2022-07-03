1b0a2ba21176a11d5ecfa3a1fb7d994d964a4aca
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-07-03 10:16:04 +0530

    Added upload data

---
+++ workflows/upload_data.py
@@ -0,0 +1,32 @@
+import os
+
+from utils import File, logx, timex
+
+from geo2 import common, gbox_tree
+
+log = logx.get_logger('geo2.workflows.upload_data')
+
+
+def build_readme():
+    time_id = timex.get_time_id()
+    lines = [
+        '# Geo2',
+        '*Various geospatial utilities*',
+        '---',
+        f'Last updated at **{time_id}**',
+    ]
+    readme_file = os.path.join(common.DIR_DATA, 'README.md')
+    File(readme_file).write('\n\n'.join(lines))
+    log.info(f'Wrote {readme_file}')
+
+
+def upload_data():
+    common.init_dir_data()
+    gbox_tree.GBoxTree('province', 2)
+    gbox_tree.GBoxTree('district', 3)
+    gbox_tree.GBoxTree('dsd', 4)
+    build_readme()
+
+
+if __name__ == '__main__':
+    upload_data()
