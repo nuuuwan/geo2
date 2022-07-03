8d8d1693c7799b518ca78742d90f5b9e1c4153cc
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-07-03 09:59:13 +0530

    Changed to district

--- tests/geo2/test_gbox_tree.py
+++ tests/geo2/test_gbox_tree.py
@@ -5,13 +5,13 @@ from geo2 import gbox_tree
 
 class TestCase(unittest.TestCase):
     def test(self):
-        tree = gbox_tree.GBoxTree('province', 1)
+        tree = gbox_tree.GBoxTree('district', 2)
         for [lnglat, expected_region_ids] in [
-            [[79.86481964805327, 6.917311842223569], ['LK-1']],
-            [[80.63664111018507, 7.311872685858744], ['LK-2']],
-            [[80.21636619668217, 6.027218412807919], ['LK-3']],
-            [[80.01192181056106, 9.662254850528123], ['LK-4']],
-            [[81.56060482552991, 7.927743036188908], ['LK-5']],
+            [[79.86481964805327, 6.917311842223569], ['LK-11']],
+            [[80.63664111018507, 7.311872685858744], ['LK-21']],
+            [[80.21636619668217, 6.027218412807919], ['LK-31']],
+            [[80.01192181056106, 9.662254850528123], ['LK-41']],
+            [[81.56060482552991, 7.927743036188908], ['LK-51']],
         ]:
             actual_region_ids = tree.find_regions(lnglat)
             self.assertEqual(expected_region_ids, actual_region_ids)
